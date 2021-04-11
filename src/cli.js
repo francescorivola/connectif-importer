a305ed5bddcdc669d3632ab69b62097e997452e2
Francesco Rivola <francesco.rivola@gmail.com>
2021-04-11 16:57:58 +0200

    added strict mode

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const fetch = require('node-fetch')
 const fs = require('fs')
 const FormData = require('form-data')
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+'use strict'
+
 const cli = require('./cli')
 
 cli().parseAsync(process.argv).catch(error => {
